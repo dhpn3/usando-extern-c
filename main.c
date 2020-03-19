68e5c9722a46810050ee5e586e3a0832ff3ae521
Djhonathan Paiva <djhonathanhenriquepaiva@gmail.com>
2020-03-18 22:54:42 -0300

    Quebra de linha

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include "calculadora.h";
 
 int main(void)  {
-    printf("hello");
-    printf("A soma eh: %.2f", soma(2, 2));
-    printf("A sub eh: %.2f", sub(2, 2));
-    printf("A divisao eh: %.2f", divison(2, 2));
-    printf("A multiplicacao eh: %.2f", mult(2, 2));
+    printf("hello\n");
+    printf("A soma eh: %.2f\n", soma(2, 2));
+    printf("A sub eh: %.2f\n", sub(2, 2));
+    printf("A divisao eh: %.2f\n", divison(2, 2));
+    printf("A multiplicacao eh: %.2f\n", mult(2, 2));
     return 1;
 }
